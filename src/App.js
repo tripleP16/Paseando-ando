a02a05acdff6f51bbaef5612e972c55f5f1f5738
pablo <perez51160900@hotmail.com>
2021-04-05 17:29:32 -0400

    gg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-export default class App extends React.Component{
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.css'
+import Place from './Place';
+import Rating from './Rating';
+import Horario from './Horario'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={photo:''}
+  }
+  map=''
+
+  componentDidMount(){
+    const googlePlaceAPILoad = setInterval(() => {
+      if (window.google){
+        this.google=window.google;
+        clearInterval(googlePlaceAPILoad);
+        console.log('Load Place API');
+        const mapCenter = new this.google.maps.LatLng(4.624335,-74.064644);
+        this.map = new this.google.maps.Map(document.getElementById('gmapContainer'), {
+          center: mapCenter,
+          zoom: 15
+        });
+        var marcador = new this.google.maps.Marker({position:mapCenter, map:this.map})
+        this.showMap(mapCenter);
+      };
+    },100);
+  }
+
+  showMap(mapCenter) {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new window.google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: mapCenter});
+    // The marker, positioned at Uluru
+    var marker = new window.google.maps.Marker({position: mapCenter, map: map});
+  }
+
+  manejoOnClick = (e) => {
+    const request = {
+      query: document.getElementById('origen').value ,
+      fields: ['photos', 'formatted_address', 'name','place_id'],
+    };
+    this.service = new this.google.maps.places.PlacesService(this.map);
+    this.service.findPlaceFromQuery(request, this.findPlaceResult);
+  }
+
+  findPlaceResult = (results, status) => {
+    var placesTemp=[]
+    var placeId = ''
+    if (status ===  'OK') {
+      results.map((place) => {
+        var placePhotos=['']
+        const placeTemp = {id:place.place_id, name:place.name,
+          address:place.formatted_address,photos:placePhotos}
+          placeId = place.place_id;
+        placesTemp.push(<Place placeData={placeTemp}/>);
+      })
+    }
+    if (placesTemp.length>0)
+      this.findPlaceDetail(placeId);
+    else{
+      const placeTemp = {id:'N/A', name:<div className='mt-5'><strong className='text-center'>
+          No hay resultados</strong></div>,
+        address:'',photos:['']}
+      placesTemp.push(<Place placeData={placeTemp}/>);
+      this.setState({places:placesTemp})
+    }
+  }
+
+  findPlaceDetail = (placeIdFound) => {
+    var request = {
+      placeId: placeIdFound,
+      fields: ['address_component', 'adr_address', 'alt_id', 'formatted_address',
+       'icon', 'id', 'name', 'permanently_closed', 'photo', 'place_id', 'plus_code', 'scope', 
+       'type', 'url', 'utc_offset', 'vicinity','geometry','rating','reviews','opening_hours']
+    };
+    this.service.getDetails(request, this.foundPlaceDatail);
+  }
+
+  foundPlaceDatail = (place, status) => {
+    if (status === 'OK'){
+      var placePhotos=['']
+      if (place.photos){
+        place.photos.map((placePhoto, index) => {
+          placePhotos[index]=placePhoto.getUrl({'maxWidth': 160, 'maxHeight': 120})
+          if (index === 2) return;
+        })
+      }
+      const placeTemp = {id:place.place_id, name:place.name,
+        address:place.formatted_address,photos:placePhotos}
+      const placesTemp = <Place placeData={placeTemp}/>;
+      const placeHorarios = <Horario horarios={place.opening_hours}/>
+      var rating=''
+      if (place.rating){
+        rating = <Rating placeRating={place.rating} placeReviews={place.reviews}/>
+      }
+      console.log('address_component: '+ place.address_component, 
+      'adr_address: '+place.adr_address, 'alt_id', 'formatted_address', 'geometry: '+place.geometry,
+      'icon: '+place.icon, 'permanently_closed', 'photo',' rating: '+place.rating,
+      'type: '+place.type, 'url: '+place.url, 'utc_offset', 'vicinity')
+      this.setState({places:placesTemp, 
+                     placeRating:rating,
+                     placeHorarios:placeHorarios})
+      this.showMap(place.geometry.location);
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" >
+
+        <div className='container border rounded p-3 mt-4' style={{width:'50%'}}>
+          <div className='row'>
+            <div className='col-4'></div>
+            <div className='col-4 text-center'>
+              <label><strong>Indica el lugar</strong></label>
+            </div>
+            <div className='col-4'></div>
+          </div>
+          <div className='row'>
+            <div className='col-4'></div>
+            <div className='col-4 py-2'><input id='origen' type='text'/></div>
+            <div className='col-4'></div>
+          </div>
+          <div className='row'>
+            <div className='col-4'></div>
+            <div className='col-4 text-center'>
+              <div className='btn btn-primary text-center' onClick={this.manejoOnClick}>Buscar Lugar</div>
+            </div>
+            <div className='col-4'></div>
+          </div>
+          {this.state.places}
+          {this.state.placeHorarios}
+          {this.state.placeRating}
+          <div id="map"></div>
+        </div>
+        
       </div>
     );
   }
 }
+
+export default App;
